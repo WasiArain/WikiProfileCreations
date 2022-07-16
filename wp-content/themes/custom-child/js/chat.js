50e526646bf46375f6f8c4d79f6ce1aa208e0e67
WasiArain <chaudhrywasiullah807@gmail.com>
2022-07-17 04:30:23 +0500

    Code updated

--- wp-content/themes/custom-child/js/chat.js
+++ wp-content/themes/custom-child/js/chat.js
@@ -4,7 +4,7 @@ var Tawk_API = Tawk_API || {},
     var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/5c75247fa726ff2eea597f4f/default';
+    s1.src = 'https://embed.tawk.to/62d17165b0d10b6f3e7c819e/1g8125vvo';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
